1678e5e82b9b28665b386ad1b8f067bed574f805
Earth Science Developer Juanse <137618754+earthdeveloperjuanse@users.noreply.github.com>
2025-03-18 17:01:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
+import folium
 import streamlit as st
-from arcgis.gis import GIS
-from arcgis.mapping import WebMap
 
-# Título de la aplicación
-st.title("Aplicación de Mapa con Autenticación")
+from streamlit_folium import st_folium
 
-# Solicitud de credenciales al usuario
-st.write("Por favor, ingrese sus credenciales de ArcGIS Online para acceder al mapa.")
-username = st.text_input("Nombre de usuario:")
-password = st.text_input("Contraseña:", type="password")
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
 
-# Botón de ingreso
-if st.button("Ingresar"):
-    if username and password:
-        try:
-            # Autenticarse en GIS
-            gis = GIS("https://www.arcgis.com", username, password)
-            st.success(f"Bienvenido, {username}!")
-            map = gis.map('Paris')
-            map.to_streamlit(height=700)
-        except Exception as e:
-            st.error(f"Error de autenticación. Por favor, verifique sus credenciales. {e}")
-    else:
-        st.warning("Por favor, ingrese tanto el nombre de usuario como la contraseña.")
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)

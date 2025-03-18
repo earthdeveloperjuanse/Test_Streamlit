47d83ad85def15ac61d5336dc9e896b1a90ec38e
Earth Science Developer Juanse <137618754+earthdeveloperjuanse@users.noreply.github.com>
2025-03-18 17:25:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,16 @@ import streamlit as st
 import requests
 from streamlit_folium import st_folium
 
-
-# Configurar la aplicación de Streamlit
 st.title("Mapa con Capa de Living Atlas de Esri")
-
-# Coordenadas iniciales del mapa
 center = [0, 0]  # Ubicación inicial en el centro del mapa
-
-# Crear el mapa base
 m = folium.Map(location=center, zoom_start=2)
-
-# URL de la capa FeatureServer en formato GeoJSON
 feature_layer_url = "https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Satellite_VIIRS_Thermal_Hotspots_and_Fire_Activity/FeatureServer/0/query"
-
-# Parámetros para obtener los datos en formato GeoJSON
 params = {
     "where": "1=1",  # Obtener todas las entidades
     "outFields": "*",  # Obtener todos los atributos
     "f": "geojson"  # Formato de salida en GeoJSON
 }
-
-# Hacer la solicitud a la API de ArcGIS
 response = requests.get(feature_layer_url, params=params)
-
 if response.status_code == 200:
     geojson_data = response.json()
     print(geojson_data)

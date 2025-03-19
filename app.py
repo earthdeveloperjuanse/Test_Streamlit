4a55a1a7c3ef347ddcdc69766caaf85e7c65f6a6
Earth Science Developer Juanse <137618754+earthdeveloperjuanse@users.noreply.github.com>
2025-03-18 19:10:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,29 +17,26 @@ end_date = st.sidebar.date_input("Fecha de fin", datetime.today())
 
 center = [(min_lat + max_lat) / 2, (min_lon + max_lon) / 2]
 m = folium.Map(location=center, zoom_start=5)
-
-if st.sidebar.button("Buscar"):
-    feature_layer_url = "https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Satellite_VIIRS_Thermal_Hotspots_and_Fire_Activity/FeatureServer/0/query"
-    # Prueba
-    where_clause = (
-        f"latitude >= {min_lat} AND latitude <= {max_lat} AND longitude >= {max_lon} AND longitude <= {min_lon} AND acq_date >= '{start_date}' AND acq_date <= '{end_date}'"
-    )
-    params = {
-        "where": where_clause,  # Obtener todas las entidades
-        "outFields": "*",  # Obtener todos los atributos
-        "f": "geojson",  # Formato de salida en GeoJSON
-        "resultRecordCount": 300  # Límite de 100 registros
-    }
-    response = requests.get(feature_layer_url, params=params)
-    if response.status_code == 200:
-        geojson_data = response.json()
-        folium.GeoJson(
-            geojson_data,
-            tooltip=folium.GeoJsonTooltip(fields=["bright_ti4", "confidence"], aliases=["Brillo:", "Confianza:"]),
-            popup=folium.GeoJsonPopup(fields=["bright_ti4", "confidence"], aliases=["Brillo:", "Confianza:"])
-        ).add_to(m)
-    else:
-        st.error("No se pudo cargar la capa. Verifica la URL del Feature Layer.")
-
+feature_layer_url = "https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Satellite_VIIRS_Thermal_Hotspots_and_Fire_Activity/FeatureServer/0/query"
+# Prueba
+where_clause = (
+    f"latitude >= {min_lat} AND latitude <= {max_lat} AND longitude >= {max_lon} AND longitude <= {min_lon} AND acq_date >= '{start_date}' AND acq_date <= '{end_date}'"
+)
+params = {
+    "where": where_clause,  # Obtener todas las entidades
+    "outFields": "*",  # Obtener todos los atributos
+    "f": "geojson",  # Formato de salida en GeoJSON
+    "resultRecordCount": 300  # Límite de 100 registros
+}
+response = requests.get(feature_layer_url, params=params)
+if response.status_code == 200:
+    geojson_data = response.json()
+    folium.GeoJson(
+        geojson_data,
+        tooltip=folium.GeoJsonTooltip(fields=["bright_ti4", "confidence"], aliases=["Brillo:", "Confianza:"]),
+        popup=folium.GeoJsonPopup(fields=["bright_ti4", "confidence"], aliases=["Brillo:", "Confianza:"])
+    ).add_to(m)
+else:
+    st.error("No se pudo cargar la capa. Verifica la URL del Feature Layer.")
 
 st_folium(m, width=1500, height=500)
